0fd22d981414d9321f21479e9e82cc5762f97419
Marcelo Raineri <raineridev@gmail.com>
2025-01-15 23:53:30 +0000

    feat(controller):crud in user

---
+++ app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Requests\Auth\LoginRequest;
+
+class LoginController extends Controller
+{
+    public function login(LoginRequest $request)
+    {
+        $credentials = request(['email','password']);
+        if(!Auth::attempt($credentials))
+        {
+            return response()->json([
+                'message' => 'Unauthorized'
+            ],401);
+        }
+
+        $user = $request->user();
+        $tokenResult = $user->createToken('Personal Access Token');
+        $token = $tokenResult->plainTextToken;
+
+        return response()->json([
+            'accessToken' =>$token,
+            'token_type' => 'Bearer',
+        ]);
+    }
+}
