d2966a946d65c91032d162031f7c60d1cfac244d
Marcelo Raineri <raineridev@gmail.com>
2025-01-24 18:16:17 +0000

    feat(model): has roles trait

--- app/Models/User.php
+++ app/Models/User.php
@@ -12,7 +12,7 @@ use Illuminate\Notifications\Notifiable;
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasRoles, HasApiTokens, HasFactory, Notifiable;
 
     /**
      * The attributes that are mass assignable.
