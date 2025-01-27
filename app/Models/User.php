2e4e14ac326cf3a44fd9d31ecb4b3a5a4b524e4d
Marcelo Raineri <raineridev@gmail.com>
2025-01-27 16:56:29 +0000

    feat(model):implementing permissions in the model

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,10 +4,11 @@ namespace App\Models;
 
 use App\Events\AccountCreated;
 use Laravel\Sanctum\HasApiTokens;
+use Spatie\Permission\Traits\HasRoles;
+use Illuminate\Notifications\Notifiable;
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
